00437cdc9f0596ead1e989db20b4dd2c9d92da8b
SayaliBaxy <46625157+SayaliBaxy@users.noreply.github.com>
2019-02-01 16:32:58 +0530

    Added another program

---
+++ Pattern.py
@@ -0,0 +1,14 @@
+n = input("enter number of rows")
+for i in range(1,n+1):
+    for j in range (1,i+1):
+        print("*",end="")
+    print()
+
+
+# The syntax end="" is not supported in python2.X, then what is the correct syntax
+#to be used in python2.x
+          
+        
+ 
+ 
+        
